3e993cd5049ac4c7aac0aac619b2590576a3727c
olivia <olivia@fastmail.com>
2017-08-03 18:15:43 +0200

    automatically rename to html output file

--- test/transform.test.js
+++ test/transform.test.js
@@ -3,10 +3,12 @@ const transform = require('../lib/transform')
 
 test('parses markdown', t => {
   const input = {
+    path: 'test.md',
     contents: '# Hello'
   }
 
   const expected = {
+    path: 'test.html',
     contents: '<h1>Hello</h1>\n'
   }
 

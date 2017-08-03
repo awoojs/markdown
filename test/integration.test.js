f575be70c676398f49ccd532578a28071e1d8b3f
olivia <olivia@fastmail.com>
2017-08-03 18:07:19 +0200

    add integration test

---
+++ test/integration.test.js
@@ -0,0 +1,19 @@
+const test = require('ava')
+const weh = require('@weh/weh')
+const markdown = require('../index')
+
+test('correctly parses', async t => {
+  const files = [
+    {
+      path: 'test.md',
+      contents: '# hello'
+    }
+  ]
+
+  const res = await weh.integration(async site => {
+    site.use(markdown)
+    return site
+  }, files)
+
+  t.is(res.files[0].contents, '<h1>hello</h1>\n')
+})
